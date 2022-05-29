a3fd5ebb0f91b69ef4b868d879cc0ef8f34c48df
thenavik <iamdigitalist@gmail.com>
2022-05-29 14:53:01 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-const day = document.querySelector("#day");
-const month = document.querySelector("#month");
-const year = document.querySelector("#year");
-const countBtn = document.querySelector("#count-btn");
+const form = document.querySelector("#form");
+form.addEventListener("submit", getFormValue);
+function getFormValue(event) {
+  event.preventDefault();
+  let day = form.querySelector('[name="day"]');
+  let month = form.querySelector('[name="month"]');
+  let year = form.querySelector('[name="year"]');
+
+  const data = {
+    day: day.value,
+    month: month.value,
+    year: year.value,
+  };
+
+  console.log(data);
+}
+getFormValue();
