f810d9608a3fab60ef781b45c698353841729c84
thenavik <iamdigitalist@gmail.com>
2022-05-29 14:43:07 +0300

    button

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const day = document.querySelector("#day");
+const month = document.querySelector("#month");
+const year = document.querySelector("#year");
+const countBtn = document.querySelector("#count-btn");
